648e5b28bad9fb7ea1b0c91fec0e4ce96a9fd1e1
amratallah123 <amratallahh@gmail.com>
2022-07-27 20:09:27 +0200

    rank endPoint

--- routes/tasks.js
+++ routes/tasks.js
@@ -40,4 +40,24 @@ router.get("/words", (req, res) => {
   res.send(tenObject);
 });
 
+router.post("/rank", (req, res) => {
+  if (req.body.score >= 90) {
+    res.send({
+      rank: 80,
+    });
+  } else if (req.body.score >= 60 && req.body.score < 90) {
+    res.send({
+      rank: 56.67,
+    });
+  } else if (req.body.score >= 50 && req.body.score < 60) {
+    res.send({
+      rank: 40,
+    });
+  } else if (req.body.score >= 30 && req.body.score < 50) {
+    res.send({
+      rank: 26.67,
+    });
+  }
+});
+
 module.exports = router;
