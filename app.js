b575abc4000a39d418887de1e0d4a0fcba09fa6a
amratallah123 <amratallahh@gmail.com>
2022-07-28 15:23:03 +0200

    change timelimited

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-  windowMs: 1, // 15 minutes
+  windowMs: 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
