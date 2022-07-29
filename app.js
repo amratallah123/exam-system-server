18f88afb8509cdf028fb6853f7f46269b05dbc60
amratallah123 <amratallahh@gmail.com>
2022-07-29 17:34:36 +0200

    change port number

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use("/api", tasks);
-const port = 3000;
+const port = 1000;
 
 app.listen(port, () => console.log(`Listening on Port ${port}...`));
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
 to open run the project run the following command
 1. npm install
 
-2. node app.js
+2. npm run serve
